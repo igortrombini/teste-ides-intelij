87ae78d2ba07f04937cbe339e93f6074ce22667d
igortrombini <igorts20@gmail.com>
2023-08-09 18:08:05 -0300

    Criando Classe Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,43 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-        /*int a = 5;
-        int b = 3;
-        System.out.println("hello world" + (a+b));*/
+        Livro livro1 = new Livro("A casa de Papel", 125);
+        System.out.println(livro1);
+
+    }
+}
+
+class Livro{
+    private String name;
+    private Integer numPaginas;
+
+    public Livro(String name, Integer numPaginas) {
+        this.name = name;
+        this.numPaginas = numPaginas;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "name='" + name + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
+
